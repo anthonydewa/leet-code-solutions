373bb4e11248fa75b32704e4f9ac5dc7789a231c
anthonydewa <anthonydewa168@gmail.com>
2024-05-11 20:01:33 +0700

    3. length of longest substring

--- 3.longest-substring-non-repeating/LengthOfLongestSubstring.java
+++ 3.longest-substring-non-repeating/LengthOfLongestSubstring.java
@@ -1,7 +1,28 @@
+import java.util.HashMap;
+
 class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        
+        int count = 0;
+        HashMap<Character, Integer> test = new HashMap<Character, Integer>();
 
-        return 0;
+        for (int i = 0; i < s.length(); i++) {
+            test.put(s.charAt(i), 1);
+            for (int j = (i + 1); j < s.length(); j++) {
+                Character cs = s.charAt(j);
+
+                if (test.get(cs) == null) {
+                    test.put(cs, 1);
+                } else {
+                    break;
+                }
+            }
+
+            if (count < test.size()) {
+                count = test.size();
+            }
+            test.clear();
+        }
+
+        return count;
     }
 }
