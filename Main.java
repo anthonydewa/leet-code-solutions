a1493184bb8246c587a81e3ab000e4b640176a38
anthonydewa <anthonydewa168@gmail.com>
2024-05-29 10:18:59 +0700

    update hierarchy

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // longest substring
-        PalindromeNumber pn = new PalindromeNumber();
-
-        boolean pn = pn.isPalindrome(123);
         System.out.println(pn);
     }
 }
